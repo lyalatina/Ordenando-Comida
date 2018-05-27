dbdbe397f2ed5e1d154e39335add2c1d9e2d36e1
Natalia <nataliamp87@gmail.com>
2018-05-27 17:44:45 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+//Comenzando if
+
+let ceviche = prompt('Cual es tu pedido');
+if(ceviche === 'ceviche') {
+  alert('No hay ceviche');
+  
+}
+
+let tacosalpastor = prompt('Ordena otro plato ');
+if (tacosalpastor === 'tacos al pastor') {
+  alert("no hay tacos al pastor");
+}
+
+let empanadachilena = prompt('Ordena otro plato');
+if (empanadachilena === 'empanada chilena') {
+  alert("no hay empanada chilena");
+}
+
+let hamburguesa = prompt('Ordena otro plato');
+if (hamburguesa === 'hamburguesa') {
+
+    alert("si hay hamburguesa");
+
+  }
+
+}
+  
